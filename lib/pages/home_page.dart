caea58efa06a5bef8fbc7a2e06eaf62117b351ae
sohibasarmonova <sohibasarmonova@gmail.com>
2024-03-20 20:21:18 +0500

    home page and collection page added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:unsplash_app/pages/collection_page.dart';
+import 'package:unsplash_app/pages/search_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,13 +11,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0; //New
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    const List<Widget> _pages = <Widget>[
+
+      SearchPage(),
+      CollectionPage()
+    ];
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Unsplash'),
+
+      body: Center(
+        child: _pages.elementAt(_selectedIndex), //New
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.collections), label: ''),
+        ],
+        currentIndex: _selectedIndex, //New
+        onTap: _onItemTapped,
       ),
     );
+
   }
 }
