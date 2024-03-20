69b3d9878ee1106659632e443cee7c41a1ced59c
sohibasarmonova <sohibasarmonova@gmail.com>
2024-03-20 21:25:58 +0500

    search textField has been added to the home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -28,7 +28,30 @@ class _HomePageState extends State<HomePage> {
     ];
     return Scaffold(
 
+      appBar: AppBar(
+        title: Container(
+
+          height: 46,
+          padding: EdgeInsets.only(left: 15, right: 15),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(23),
+              border: Border.all(
+                  width: 1,
+                  color: Colors.grey.withOpacity(0.5))),
+
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: "Search photos, collections,users",
+                border: InputBorder.none,
+                prefixIcon:Icon(Icons.search,color: Colors.grey,) ,
+                hintStyle: TextStyle(color: Colors.grey[700])),
+
+          ),
+        ),
+      ),
+
       body: Center(
+
         child: _pages.elementAt(_selectedIndex), //New
       ),
       bottomNavigationBar: BottomNavigationBar(
